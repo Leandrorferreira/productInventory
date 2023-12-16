47b04d399e60dfb6fee9981508174f00f5515615
leandro.rferreira <leandro.rferreira@sp.senac.br>
2023-12-15 22:27:01 -0300

    remove unnecessary imports

--- src/main/java/com/inventory/productInventory/controller/CategoryController.java
+++ src/main/java/com/inventory/productInventory/controller/CategoryController.java
@@ -7,9 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RequiredArgsConstructor
